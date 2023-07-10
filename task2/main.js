2deaae93dcdfcc093e7658727147d95d0be4f912
Munthera <alaesmohammad88@gmail.com>
2023-07-10 19:25:01 +0300

    new

--- task2/main.js
+++ task2/main.js
@@ -1,25 +1,25 @@
-let operation =prompt("enter the operation (+, - , * , /):");
-let num1 = number(prompt("enter the first number:"));
-let num2 = number(prompt("enter the seconde number:"));
+let operation = prompt("enter the operation (+, - , * , /):");
+let num1 = Number(prompt("enter the first number:"));
+let num2 = Number (prompt("enter the seconde number:"));
 
 let result;
 
 if(operation === "+"){
     result = num1 + num2;
-}
-else if(operation === "-"){
+ }
+    else if(operation === "-"){
     result = num1 - num2;
-}
-else if(operation === "*"){
+    }
+        else if(operation === "*"){
     result = num1 * num2;
-}
+    }
 
-else if(operation === "/"){
+        else if(operation === "/"){
     result = num1 / num2;
-}
-else{
+    }
+    else{
     console.log("invalid operation !");
-}
+    }
 
 if (result !== undefined){
     console.log("The result is:",result);
