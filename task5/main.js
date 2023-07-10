de7a175064f2f252892a4713a5ad1bb5a414d098
Munthera <alaesmohammad88@gmail.com>
2023-07-11 02:22:39 +0300

    new

---
+++ task5/main.js
@@ -0,0 +1,14 @@
+
+let number = 1;
+let line = ' ';
+for(i = 1 ; i <= 4 ; i++ ){
+    line = ' ';
+
+
+    for(m = 1 ; m <= i ; m++){
+     line += number +' ' ;
+        number++
+    }
+       
+   console.log(line );
+}
